c6edd80d52777455a6f40c9c2d5de9380268823e
Kasper Nissen <kni@it-minds.dk>
2016-02-22 21:42:04 +0100

    Initial commit

---
+++ src/main/java/com/rpicloud/CasestudyGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.rpicloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasestudyGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasestudyGatewayApplication.class, args);
+	}
+}
